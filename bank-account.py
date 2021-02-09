320a2311a0e5053c2da4131ec4c350eff07fac5f
MaybeJaybe <69604238+MaybeJaybe@users.noreply.github.com>
2021-02-09 00:15:39 -0800

    added atm

--- bank-account.py
+++ bank-account.py
@@ -17,29 +17,29 @@ class BankAccount:
     # methods def
     def deposit(self, amount):
         # take parameter amount, add amount to the balance
+        amount = float(amount)
         self.balance += amount
-        total = float(amount)
-
-        print(f"Amount Deposited: ${total}")
-        print(f"New Balance: ${self.balance}")
+        print(f"Amount Deposited: ${amount}")
+        print(" ")
 
     def withdraw(self, amount):
         # take parameter amount, subtract amount from balance
-        if self.balance >= amount:
-            self.balance -= amount
-            total = float(amount)
-            print(f"Amount Withdrawn: ${total}")
-            print(f"New Balance: ${self.balance}")
+        amount = float(amount)
+        self.balance -= amount
+        if self.balance >= 0:
+            print(f"Amount Withdrawn: ${amount}")
+            print(" ")
         else:
-            self.balance -= amount
             # if user withdraws more than current balance charge overdraft fee of $10
             self.balance -= 10
             print("Insufficient funds. An overdraft fee of $10.00 has been charged to your account.")
-            print(f"New Balance: ${self.balance}")
+            print(" ")
 
     def get_balance(self):
+        self.rounded = str(round(self.balance, 2))
         print("Thank you for using The Bank of Dad.")
-        print(f"Your balance is ${self.balance}. You should get a job like your sister.")
+        print(f"Your balance is ${self.rounded}. You should get a decent job like your cousin.")
+        print(" ")
         return self.balance
 
     def add_interest(self):
@@ -47,52 +47,78 @@ class BankAccount:
         interest = self.balance * 0.00083
         self.interest = interest
         self.balance += self.interest
-        print(self.interest)
-        print(self.balance)
+        return self.balance
 
     def print_reciept(self):
+        self.rounded = str(round(self.balance, 2))
         print(" ")
         print("Account Summary:")
         print(" ")
         print("Name:", self.full_name)
         print("Routing Number:", self.routing_number)
         print("Account Number:", self.account_number)
-        print("Balance: $", str(round(self.balance, 2)))
+        print(f"Balance: ${self.rounded}")
+        print(" ")
+
+# atm screens
+def login_screen(account_list):
+    print("Welcome to The Bank of Dad ATM")
+    name = input("Enter your name to log in or press 0 to exit: ")
+    if name == "0":
+        return -1
+    for i in range(len(account_list)):
+        if name == account_list[i].full_name:
+            return i
+    return None
+
+def atm_menu():
+    print("Menu")
+    print("Press 1 to make a deposit")
+    print("Press 2 to make a withdrawl")
+    print("Press 3 to check your balance")
+    print("Press 4 for account summary")
+    print("Press 5 to log out")
+    print("Press 0 to exit")
+    return input("Make your selection: ")
+
 
 # outside class, define 3 different bank acct examples using BankAccount object
+if __name__ == "__main__":
+    # this clears console log
+    print(chr(27) + "[2J")
 
-jay = BankAccount("Jay MayMay")
-ares = BankAccount("Ares Allan")
-lexi = BankAccount("Lexi Lou-Ann")
+    account_list = []
+    account_list.append(BankAccount("Jay MayMay"))
+    account_list.append(BankAccount("Ares Allan"))
+    account_list.append(BankAccount("Lexi Lou-Ann"))
 
-print(jay.full_name)
-print(f"Account Number: {str(jay.account_number)}")
-print(f"Routing Number: {jay.routing_number}")
+    profile = None
 
-print(ares.full_name)
-print(f"Account Number: {str(ares.account_number)}")
-print(f"Routing Number: {ares.routing_number}")
+    while True:
+        if not profile:
+            profile = login_screen(account_list)
+            if not profile:
+                print("That is not the name of my child, try again.")
+                print(" ")
+            if profile == -1:
+                break
 
-print(lexi.full_name)
-print(f"Account Number: {str(lexi.account_number)}")
-print(f"Routing Number: {lexi.routing_number}")
-
-
-jay.deposit(10)
-jay.withdraw(10)
-ares.deposit(100)
-jay.withdraw(10)
-ares.withdraw(45)
-jay.get_balance()
-ares.get_balance()
-jay.print_reciept()
-ares.print_reciept()
-lexi.print_reciept()
-
-jay.add_interest()
-ares.add_interest()
-lexi.add_interest()
-
-jay.print_reciept()
-ares.print_reciept()
-lexi.print_reciept()
+        if profile:
+            selection = atm_menu()
+            if selection == "1":
+                deposit_amount = input("How much do you want to deposit? ")
+                account_list[profile].deposit(deposit_amount)
+            elif selection == "2":
+                withdraw_amount = input("How much would you like to withdraw? ")
+                account_list[profile].withdraw(withdraw_amount)
+            elif selection == "3":
+                account_list[profile].get_balance()
+            elif selection == "4":
+                account_list[profile].print_reciept()
+            elif selection == "5":
+                profile = None
+            elif selection == "exit" or selection == "0":
+                break
+            else:
+                print("Error. Dad does not compute. Try again.")
+                print(" ")
